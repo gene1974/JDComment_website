d3033dd0cf89c257720422c6b4b9aab4b522f725
gene1974 <842491684@qq.com>
2022-12-08 23:32:00 +0800

    add kg query

--- frontend/src/views/knowledgegraph/index.vue
+++ frontend/src/views/knowledgegraph/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div style="background:#F2F3F5 ;min-width:fit-content;">
         <div>
-            <el-row :gutter="20" class="Background">
+            <!-- <el-row :gutter="20" class="Background">
                 <h2 style="margin-left:40px">图谱查询</h2>
                 <el-row :gutter="20" style="margin:10px">
                     <el-col :span="4"><div class="xuanzekuangshuoming">查询类型</div></el-col>
@@ -11,7 +11,7 @@
                         </el-select>
                     </el-col>
                 </el-row>
-            </el-row>
+            </el-row> -->
             <el-row id="SearchNode" class="Background" :gutter="10" style="margin-left: 10px; padding-bottom: 20px;">
                 <h4 style="margin-left:20px">实体查询</h4>
                 <el-col :span="18">
@@ -33,9 +33,30 @@
                     <el-button type="primary" value="Submit" @click="search_node" class="InfoButton">查询</el-button>
                 </el-col>
             </el-row>
-            <el-row id="SearchTriplet" class="Background" :gutter="20" style="margin-left: 10px" hidden>
+            <el-row id="SearchNode" class="Background" :gutter="10" style="margin-left: 10px; padding-bottom: 20px;">
+                <h4 style="margin-left:20px">图谱查询</h4>
+                <el-col :span="18">
+                    <el-row :gutter="20">
+                        <el-col :span="4"><div class="xuanzekuangshuoming">查询问题</div></el-col>
+                        <el-col :span="9">
+                            <el-select v-model="query" placeholder="请选择">
+                                <el-option v-for="item in queryList" :key="item" :label="item" :value="item"></el-option>
+                            </el-select>
+                        </el-col>
+                        <el-col :span="4"><div class="xuanzekuangshuoming">输入文本</div></el-col>
+                        <el-col :span="5">
+                            <el-input type="text" v-model="queryentity" placeholder="花生" @input="input_text"></el-input>
+                            <p id="entity_alert" hidden style="color:red;margin-left: 5px;">请输入查询内容！</p>
+                        </el-col>
+                    </el-row>
+                </el-col>
+                <el-col :span="4">
+                    <el-button type="primary" value="Submit" @click="search_query" class="InfoButton">查询</el-button>
+                </el-col>
+            </el-row>
+            <!-- <el-row id="SearchTriplet" class="Background" :gutter="20" style="margin-left: 10px" hidden>
                 <h4 style="margin-left:40px">三元组查询</h4>
-                <!-- <el-col :span="18">
+                <el-col :span="18">
                     <el-row :gutter="20">
                         <el-col :span="4"><div class="xuanzekuangshuoming">三元组</div></el-col>
                         <el-col :span="10">
@@ -49,19 +70,17 @@
                         </el-col>
                     </el-row>
                     <div style="margin-top:20px"><b>查询实体：{{entity}}</b></div>
-                </el-col> -->
+                </el-col>
                 <el-col :span="4">
                     <el-button type="primary" value="Submit" @click="search_triplet" class="InfoButton">查询</el-button>
                 </el-col>
-            </el-row>
+            </el-row> -->
             <el-row :gutter="20">
                 <el-col :span="20" class="Background" style="margin-left: 20px">
-                    <h4 style="margin:20px">查询结果</h4>
-                    <div style="margin:20px">查询实体：{{entity}}</div>
                     <el-row>
                         <div id="result"></div>
                         <div>
-                            <iframe id="visual" :src="visualUrl" frameborder="0" style="width:600px;height:700px;"></iframe>
+                            <iframe id="visual" :src="visualUrl" frameborder="0" style="width:700px;height:800px;"></iframe>
                         </div>
                     </el-row>
                 </el-col>
@@ -81,12 +100,40 @@
             typeList: ['产品', '评价对象', '评价词', '评价类别', '情感极性', '全部'],
             typeDict:{
                 '产品': 'Product',
-                '评价对象': 'Entity',
+                '评价对象': 'Target',
                 '评价词': 'Opinion',
                 '评价类别': 'Category',
                 '情感极性': 'Polarity',
                 '全部': 'All',
             },
+            typeRelationDict:{
+                'Product': '评价内容',
+                'Target': '评价',
+                'Opinion': '评价对象',
+                'Category': '评价类别',
+                'Polarity': '评价极性',
+            },
+            query: '评价对象的所有评价词',
+            queryentity: '花生',
+            queryList:[
+                '产品的所有评价对象',
+                '评价对象的所有评价词',
+                '评价词的所有评价对象',
+                '类别对应的所有评价对象',
+                // '类别对应的所有评价词',
+                '对应情感极性的所有评价词',
+            ],
+            queryDict:{
+                '产品所有评价类别': 'Product',
+                '评价对象的所有评价词': 'Target',
+                '评价词的所有评价对象': 'Opinion',
+                '类别对应的所有评价对象': 'Category',
+                '对应情感极性的所有评价词': 'Polarity',
+            },
+            relationDict:[
+                '对应产品', '评价内容', '评价', '评价对象', '评价类别', '评价内容', '评价极性', 
+                '正面评价词', '中性评价词', '负面评价词'
+            ],
             visualUrl: '/visual.html',
             visualData: 'Hello world',
         }
@@ -111,13 +158,11 @@
         },
         search_node(){
             if(this.entity == ''){ // 没有输入信息
-                // document.getElementById('entity_alert').hidden = false
-                // return
                 this.entity = '花生'
             }
             var param = {
                 mod: 'entity',
-                type: this.typeDict[this.entityType],
+                label: this.typeDict[this.entityType],
                 name: this.entity,
             }
             var param_list = []
@@ -128,6 +173,23 @@
             document.getElementById('visual').src = this.visualUrl + param_url
         },
         search_triplet(){
+
+        },
+        search_query(){
+            if(this.entity == ''){ // 没有输入信息
+                this.entity = '花生'
+            }
+            var param = {
+                mod: 'query',
+                label: this.queryDict[this.query],
+                name: this.queryentity,
+            }
+            var param_list = []
+            for (var key in param){
+                param_list.push(key + '=' + param[key])
+            }
+            var param_url = '?' + param_list.join('&')
+            document.getElementById('visual').src = this.visualUrl + param_url
 
         },
     }
